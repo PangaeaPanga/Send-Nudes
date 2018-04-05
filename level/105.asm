e288b8792a5154026683abe9fa1ea8486034de58
PangaeaPanga <37079269+PangaeaPanga@users.noreply.github.com>
2018-04-04 22:43:16 -0400

    Delete 105.asm

--- level/105.asm
+++
@@ -1,8 +0,0 @@
-init:
-	LDA #$01
-	STA $0DC1		;/ Player can carry Yoshi over levels flag. #$00 = can't carry over levels; #$01 = can carry over levels.
-
-	LDA #$0A		;\ Yoshi color. #$04=yellow; #$06=blue; #$08=red; #$0A=green. Refreshes on level change.
-	STA $13C7		;/ Mario Starts with (Green) Yoshi.
-	
-	RTL
